b9bd0b6efb245c1aae50fd5a97278f9da8bb6153
Juan Fuentes <jn.fuentest@gmail.com>
2012-03-05 22:22:06 -0430

    Registro de participantes en mesas de trabajo

--- app/models/mesa_de_trabajo.rb
+++ app/models/mesa_de_trabajo.rb
@@ -1,8 +1,8 @@
 class MesaDeTrabajo < ActiveRecord::Base
   belongs_to :ponente
   belongs_to :patrocinante
-  has_many :participantes_mesas, :foreign key => "id_mesa"
-  has_many :participantes, :through => :participantes_mesas, :foreign key => "id_par"
+  has_many :participantes_mesas, :foreign_key => "id_mesa"
+  has_many :participantes, :through => :participantes_mesas, :foreign_key => "id_par"
   
   texto_regex	= /[[a-z]+\s]+\z/i
   palabra_regex	= /\A[a-z]+\z/i
