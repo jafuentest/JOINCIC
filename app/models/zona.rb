96d0e042359a46ed9810068964cdb1ecb6bf2b3f
Juan A. Fuentes T <jn.fuentest@gmail.com>
2012-03-07 21:00:40 -0430

    Se me olvidó agregar unos archivos para el manejo de zonas

--- app/models/zona.rb
+++ app/models/zona.rb
@@ -1,8 +1,14 @@
 class Zona < ActiveRecord::Base
-  has_many :participantes, :foreign_key => "id_zona"
+  has_many :participantes
   
   palabra_regex	= /\A[a-z]+\z/i
   
-  validates :nombre,    :format => { :with => palabra_regex }
+  validates :nombre,    :format => { :with => palabra_regex },
+                        :uniqueness => true
+                        
   validates :capacidad, :presence => true
+  
+  def disponibilidad
+    capacidad - participantes.count
+  end
 end
