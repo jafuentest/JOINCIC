7ca4f15e5d603bff43d5e2a2e6847fdab189b493
Juan A. Fuentes T <jn.fuentest@gmail.com>
2012-03-07 21:07:49 -0430

    Casi está lista la parte de materiales POP, solo falta la interfaz para la entrega de materiales

--- app/models/material_pop.rb
+++ app/models/material_pop.rb
@@ -1,9 +1,21 @@
 class MaterialPop < ActiveRecord::Base
-  has_many :participantes_mates, :foreign_key => "id_mate"
-  has_many :participantes, :through => :participante_mate, :foreign_key => "id_par"
+  has_many :participantes_mates
+  has_many :participantes, :through => :participante_mate
   
   palabra_regex	= /\A[a-z]+\z/i
   
   validates :nombre,   :format => { :with => palabra_regex }
   validates :cantidad, :presence => true
+  
+  def existencia
+    cantidad - participantes_mates.count(:conditions => { :entregado => true })
+  end
+  
+  def deuda
+    participantes_mates.count(:conditions => { :entregado => false })
+  end
+  
+  def disponibilidad
+    cantidad - participantes_mates.count
+  end
 end
