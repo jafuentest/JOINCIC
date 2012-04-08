e06a4dbdbb3ee15148c774bea2776d30aa9d6440
Juan Fuentes <jn.fuentest@gmail.com>
2012-04-08 18:14:23 -0430

    Ya está completa la entrega de materiales POP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
    before_filter :estarLogueado
   
   def estarLogueado
-	if(session[:organizador] == nil)
-		#flash[:notice] = "Debes estar logueado para poder hacer cualquier cambio"
-		redirect_to inicioSesion_path
+    if(session[:organizador] == nil)
+      #flash[:notice] = "Debes estar logueado para poder hacer cualquier cambio"
+      redirect_to new_session_path
     end
   end 
   protect_from_forgery
