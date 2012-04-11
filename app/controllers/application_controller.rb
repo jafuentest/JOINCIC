95663acd52fb7cc5736adc6a2650439cfe55f9e7
Juan Fuentes <jn.fuentest@gmail.com>
2012-04-11 10:26:13 -0430

    Aviso de iniciar sesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 ﻿class ApplicationController < ActionController::Base
-   before_filter :estarLogueado
+  before_filter :estarLogueado
   
   def estarLogueado
-    if(session[:organizador] == nil)
-      #flash[:notice] = "Debes estar logueado para poder hacer cualquier cambio"
+    if session[:organizador].nil?
+      flash[:notice] = "Debe iniciar sesión para poder acceder al sistema"
       redirect_to new_session_path
     end
-  end 
+  end
+  
   protect_from_forgery
 end
