22dc8bff5fde0886fb3199988763e6af4f0962ab
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2012-05-06 14:09:43 -0430

    Configurados mensajes de aplicacion al castellano, solventado error de codificacion e mensaje de sesion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_filter :estarLogueado, :only => [:new, :create]
   layout "login"
-  
+
   def new
     @title = "Iniciar Sesion"
   end
-  
+
   def create
     organizador = Organizador.comprobarOrganizador(params[:session][:usuario],
             params[:session][:clave])
     if (!organizador)
-      flash.now[:notice] = "Error: La contraseña y el nombre de usuario no coinciden"
+      flash.now[:notice] = "Error: La clave y el nombre de usuario no coinciden"
       @title = "Iniciar Sesion"
       render "new"
     else
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
   def destroy
     /@_current_user = session[:organizador] = nil/
     reset_session
