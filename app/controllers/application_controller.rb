b9ce6845e09530500891d19cf2f4134b8b6cd7c0
Juan A. Fuentes T <jn.fuentest@gmail.com>
2012-05-06 00:48:09 -0430

    Cualquier vaina

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
-﻿class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
   before_filter :estarLogueado
   
   def estarLogueado
     if session[:organizador].nil?
-      flash[:notice] = "Debe iniciar sesión para poder acceder al sistema"
+      flash[:notice] = "Debe iniciar sesion para poder acceder al sistema"
       redirect_to new_session_path
     end
   end
