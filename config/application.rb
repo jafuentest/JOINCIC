fe932c5451c65430ac3baf1efeca987944e38066
Marco Gonzalo Gómez Pérez <marcogonzalo@gmail.com>
2012-05-06 14:09:43 -0430

    Configurados mensajes de aplicacion al castellano, solventado error de codificacion e mensaje de sesion

--- config/application.rb
+++ config/application.rb
@@ -31,13 +31,13 @@ module Sistema
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :es_VE
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:clave]
 
     # Enable the asset pipeline
     config.assets.enabled = true
