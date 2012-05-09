36fe30e9af3a148f40013df435940e676d3b7d13
Juan A. Fuentes T <jn.fuentest@gmail.com>
2012-05-08 23:39:54 -0430

    Se corrigió el problema con los caracteres unicode en campos validados por expresiones regulares

--- app/models/ponente.rb
+++ app/models/ponente.rb
@@ -3,20 +3,21 @@ class Ponente < ActiveRecord::Base
   has_many :ponencias
 
   email_regex	= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  texto_regex   = /\A[a-z ]+\z/i
-  palabra_regex	= /\A[a-z]+\z/i
+  texto_regex   = /\A[a-z ÁÉÍÓÚÑáéíóúñ]+\z/i
+  palabra_regex	= /\A[a-zÁÉÍÓÚÑáéíóúñ]+\z/i
   
-  validates :cedula,        :numericality => true
-  
+  validates :cedula,        :numericality => true,
+                            :uniqueness => true
+                            
   validates :nombre,        :format => { :with => palabra_regex }
   
   validates :seg_nombre,    :allow_blank => true,
                             :format => { :with => palabra_regex }
   
-  validates :apellido,      :format => { :with => palabra_regex }
+  validates :apellido,      :format => { :with => texto_regex }
   
   validates :seg_apellido,  :allow_blank => true,
-                            :format => { :with => palabra_regex }
+                            :format => { :with => texto_regex }
   
   validates :telefono,      :numericality => true,
                             :length => { :is => 11}
