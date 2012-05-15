510af0dc2994cddd0fef903c6781387f760522a3
Juan A. Fuentes T <jn.fuentest@gmail.com>
2012-05-15 00:06:22 -0430

    Se agregó la funcionalidad de reordenar la lista de participantes según sea necesario

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
 		base_title = "Sistema JOINCIC"
     base_title if @title.nil?
 	end
+  
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction, :uni => params[:uni]}, {:class => css_class}
+  end
 end
