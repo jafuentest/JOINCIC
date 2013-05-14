415ede0113795b1d38b0c5cca8251e1a8d979fcf
Juan Fuentes <jn.fuentest@gmail.com>
2013-05-14 15:07:36 -0430

    Se efectuaron cambios en las incripciones de Mesas de Trabajo y en la entrega de Material POP, hay que revisar que no se haya escapado ningún error

--- app/controllers/participantes_mesas_controller.rb
+++ app/controllers/participantes_mesas_controller.rb
@@ -1,11 +1,33 @@
 class ParticipantesMesasController < ApplicationController
-  # GET /participantes_mesas
-  # GET /participantes_mesas.json
-  def index
+  # GET /participantes_mesas/new
+  # GET /participantes_mesas/new.json
+  def new
+    numero_regex = /^[0-9]+$/
+    
     if params.has_key?(:cedula) && params[:cedula] != ""
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json => @participantes_mesas }
+      if params[:cedula] =~ numero_regex
+        @participante = Participante.find_by_cedula(params[:cedula])
+        if @participante.nil?
+          flash[:notice] = "No se encontró ningún participante cuya cédula sea: <br/>" + params[:cedula]
+          redirect_to buscar_participantes_mesas_path
+        elsif @participante.eliminado
+          flash[:notice] = "Error: El participante fue eliminado del sistema"
+          redirect_to buscar_participantes_mesas_path
+        else
+          @participantes_mesas = @participante.participantes_mesas
+          respond_to do |format|
+            if @participantes_mesas.size > 0
+              format.html { render "edit.html.erb" }
+            else
+              @mesas_de_trabajo = MesaDeTrabajo.find(:all, :conditions => {:dia => Time.now.strftime("%Y-%m-%d")})
+              @prioridad_maxima = @mesas_de_trabajo.size
+              format.html { render "new.html.erb" }
+            end
+          end
+        end
+      else
+        flash[:notice] = "Error: Número de cédula inválido"
+        redirect_to buscar_participantes_mesas_path
       end
     else
       flash[:notice] = "Ingresa el número de cédula del participante"
@@ -13,56 +35,10 @@ class ParticipantesMesasController < ApplicationController
     end
   end
 
-  # GET /participantes_mesas/1
-  # GET /participantes_mesas/1.json
-  def show
-    numero_regex = /^[0-9]+$/
-    
-    if params[:id] =~ numero_regex
-      @participante = Participante.find_by_cedula(params[:id])
-      
-      if @participante.nil?
-        flash[:notice] = "No se encontró ningún participante cuya cédula sea: <br/>" + params[:cedula]
-        redirect_to buscar_participantes_mesas_path
-      else
-        @participantes_mesas = @participante.participantes_mesas
-        
-        if @participante.eliminado
-          flash[:notice] = "Error: El participante fue eliminado del sistema"
-          redirect_to buscar_participantes_mesas_path
-        else
-          respond_to do |format|
-            if @participantes_mesas.size > 0
-              format.html { render "index.html.erb" }
-            else
-              @mesas_de_trabajo = MesasDeTrabajo.all
-              format.html { render "new.html.erb" }
-            end
-          end
-        end
-      end
-    
-    else
-      flash[:notice] = "Error: Número de cédula inválido"
-      redirect_to buscar_participantes_mesas_path
-    end
-  end
-
-  # GET /participantes_mesas/new
-  # GET /participantes_mesas/new.json
-  def new
-    @participante = Participante.find_by_cedula(params[:cedula])
-    @mesas_de_trabajo = MesaDeTrabajo.find(:all, :conditions => {:dia => Time.now.strftime("%Y-%m-%d")})
-    @prioridad_maxima = @mesas_de_trabajo.count
-    
-    respond_to do |format|
-      format.html # new.html.erb
-    end
-  end
 
   # POST /participantes_mesas
   # POST /participantes_mesas.json
-  def crear
+  def create
     @mesas_de_trabajo = MesaDeTrabajo.all
     @participante = Participante.find_by_cedula(params[:cedula])
     
@@ -80,18 +56,6 @@ class ParticipantesMesasController < ApplicationController
     
     respond_to do |format|
       format.html { render "index.html.erb" }
-    end
-  end
-  
-  # DELETE /participantes_mesas/1
-  # DELETE /participantes_mesas/1.json
-  def destroy
-    @participante_mesa = ParticipanteMesa.find(params[:id])
-    @participante_mesa.destroy
-
-    respond_to do |format|
-      format.html { redirect_to participantes_mesas_url }
-      format.json { head :ok }
     end
   end
 
