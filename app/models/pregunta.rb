7c44abcdfd701b2f25ad6c3ce4417736287d6416
Juan A. Fuentes T <jn.fuentest@gmail.com>
2013-05-25 00:00:50 -0430

    Cambio menor en la validación de las preguntas.

--- app/models/pregunta.rb
+++ app/models/pregunta.rb
@@ -11,11 +11,12 @@
 
 class Pregunta < ActiveRecord::Base
   scope :aceptada, where(:aceptada => true)
-
+  
   belongs_to :participante
   belongs_to :ponencia
-
-  validates :mensaje, :presence => true, :length => { :in => 5..255 }
+  
+  validates :mensaje, :presence => true,
+                      :length => { :maximum => 255 }
+                              
   validates :participante_id, :presence => true
-
 end
