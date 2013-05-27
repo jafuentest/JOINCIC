9a6d337e0d4719a5f0dd885a851e112e56dcab46
Juan Fuentes <jn.fuentest@gmail.com>
2013-05-27 02:56:24 -0430

    probando

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 require "will_paginate"
 
-# Initialize the rails application
-Sistema::Application.initialize!
-
-
 # Variables de entorno #
 # -------------------- #
 SALT = "J0iNcIC"
@@ -20,3 +16,7 @@ PALABRA_REGEX = /\A[a-zÁÉÍÓÚÑáéíóúñ]+\z/i
 TEXTO_REGEX   = /\A[a-z\d ÁÉÍÓÚÑáéíóúñ,.]+\z/i
 NOMBRES_REGEX = /\A[a-z ÁÉÍÓÚÑáéíóúñ]+[a-zÁÉÍÓÚÑáéíóúñ]+\z/i
 LOGIN_REGEX   = /\A[a-z\d\-_]+\z/i
+
+# Initialize the rails application
+Sistema::Application.initialize!
+
