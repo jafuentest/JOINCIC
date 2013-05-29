5dcb70056f720e37c60d14e25cb4de2e1850340f
obandox <obando777@gmail.com>
2013-05-29 05:19:41 -0430

    prueba de content XD

--- app/controllers/programas_controller.rb
+++ app/controllers/programas_controller.rb
@@ -72,7 +72,7 @@ class ProgramasController < ApplicationController
       
       data+=[{'id'=>p.id ,'case'=>caseName,'language'=>language, 'filename'=> filename, 'content'=>content}]
     end
-    render :text  =>   content
+    render :text  =>   data[0]['content']
   end
   def validar
     #confiemos ciegamente xDD :P 
