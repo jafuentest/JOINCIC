6879013c3724ca458625381b7dc5607c3dd9c736
Juan Fuentes <jn.fuentest@gmail.com>
2015-05-20 10:54:36 -0430

    Quitar información no usada de organizadores

--- app/models/organizador.rb
+++ app/models/organizador.rb
@@ -5,16 +5,10 @@
 #  id           :integer          not null, primary key
 #  usuario      :string(15)       not null
 #  clave        :string(15)       not null
-#  cedula       :integer          not null
 #  nombre       :string(15)       not null
 #  apellido     :string(15)       not null
-#  fecha_nac    :date             not null
-#  telefono     :string(11)       not null
 #  correo       :string(50)       not null
-#  direccion    :string(50)       not null
 #  institucion  :string(7)        not null
-#  nivel        :integer          not null
-#  tipo_nivel   :string(9)        not null
 #  coordinacion :string(15)       not null
 #  coordinador  :boolean          default(FALSE), not null
 #  seg_nombre   :string(15)
@@ -24,42 +18,32 @@
 
 class Organizador < ActiveRecord::Base
   include Persona
-  
+
   has_many :participantes
-  
+ 
   validates :usuario,       :format => { :with => LOGIN_REGEX },
                             :uniqueness => { :case_sensitive => false }
-                            
+
   validates :clave,         :presence => true,
                             :length => { :maximum => 15 }
-                            
-  validates :cedula,        :numericality => true,
-                            :uniqueness => true
-                            
+
   validates :nombre,        :format => { :with => TEXTO_REGEX }
-                            
+
   validates :seg_nombre,    :allow_blank => true,
                             :format => { :with => TEXTO_REGEX }
-                            
+
   validates :apellido,      :format => { :with => TEXTO_REGEX }
-                            
+
   validates :seg_apellido,  :allow_blank => true,
                             :format => { :with => TEXTO_REGEX }
-                            
-  validates :telefono,      :numericality => true,
-                            :length => { :is => 11}
-                            
+
   validates :correo,        :format => { :with => EMAIL_REGEX },
                             :uniqueness => { :case_sensitive => false }
-                            
-  validates :direccion,     :format => { :with => TEXTO_REGEX }
-                            
-  validates :nivel,         :presence => true
-  
+
   def comprobarPass(pass)
     clave == pass
   end
-  
+
   def self.comprobarOrganizador(nombre, pass)
     organizador = find_by_usuario(nombre)
       if (organizador != nil)
---
+++ db/migrate/20150520042904_remove_fields_from_organizador.rb
@@ -0,0 +1,10 @@
+class RemoveFieldsFromOrganizador < ActiveRecord::Migration
+  def change
+		remove_column :organizadores, :cedula
+		remove_column :organizadores, :fecha_nac
+		remove_column :organizadores, :telefono
+		remove_column :organizadores, :direccion
+		remove_column :organizadores, :nivel
+		remove_column :organizadores, :tipo_nivel
+  end
+end
