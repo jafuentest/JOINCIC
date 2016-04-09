e65dab802d8841855312ffc52b659d90400f3e49
Juan Fuentes <jn.fuentest@gmail.com>
2016-04-08 19:42:14 -0430

    Pasar las constantes a un archivo aparte

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require "will_paginate"
+require 'will_paginate'
 require 'net/http'
-
-# Variables de entorno #
-# -------------------- #
-SALT = "J0iNcIC"
-ORGANIZADOR = 3
-GRUPO = 2
-PARTICIPANTE = 1
-
-#Expresiones Regulares
-EMAIL_REGEX   = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-TEXTO_REGEX   = /\A[a-z\d ÁÉÍÓÚÑáéíóúñ,.]+\z/i
-LOGIN_REGEX   = /\A[a-z\d\-_]+\z/i
 
 # Initialize the Rails application.
 Rails.application.initialize!
---
+++ config/initializers/constants.rb
@@ -0,0 +1,9 @@
+SALT = 'J0iNcIC'
+ORGANIZADOR = 3
+GRUPO = 2
+PARTICIPANTE = 1
+
+#Expresiones Regulares
+EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+TEXTO_REGEX = /\A[a-z\d ÁÉÍÓÚÑáéíóúñ,.]+\z/i
+LOGIN_REGEX = /\A[a-z\d\-_]+\z/i
