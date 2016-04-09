011c97e130ef5b92a561adcb42a455e57120d016
Juan Fuentes <jn.fuentest@gmail.com>
2016-04-08 19:43:10 -0430

    Actualizar controlador de sesiones

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     organizador = Organizador.comprobarOrganizador(params[:session][:usuario], params[:session][:clave])
     if (!organizador)
-      if Organizador.find_by_usuario.nil?
+      if Organizador.find_by_usuario(params[:session][:usuario]).nil?
         flash.now[:notice] = "El usuario no existe"
       else
         flash.now[:notice] = "La clave y el nombre de usuario no coinciden"
