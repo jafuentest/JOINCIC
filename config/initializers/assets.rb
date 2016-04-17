b836fa50b9e57296456c911853eee0297754e6cf
Juan Fuentes <jn.fuentest@gmail.com>
2016-04-17 05:53:51 -0430

    Agregar CSSs a la lista de assets a pre-compilar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( blueprint/print.css )
-Rails.application.config.assets.precompile += %w( blueprint/screen.css )
-Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w(blueprint/print.css)
+Rails.application.config.assets.precompile += %w(blueprint/screen.css)
+Rails.application.config.assets.precompile += %w(custom.css)
+Rails.application.config.assets.precompile += %w(movil.css)
+Rails.application.config.assets.precompile += %w(smoke.css)
