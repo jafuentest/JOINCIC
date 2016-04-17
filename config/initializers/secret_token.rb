1eb4f4fdbcf12826fbbafbf45e7ded76bd589c82
Juan Fuentes <jn.fuentest@gmail.com>
2016-04-17 03:53:14 -0430

    Migrar de secret_token.rb a secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Sistema::Application.config.secret_token = '3dec5906e78ef8b1d7b9623054904c54e28d6224aa135f33516b43a143e9b180d6aa4a270e7472040a86d65e01e00eff3f8f3f0f5631b9761ec64b42174f79be'
